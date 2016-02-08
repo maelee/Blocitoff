1a39924eec3611ed17e49983e445a26f38a67941
Sarah <maelee3@gmail.com>
2016-02-07 22:01:53 -0700

    Stuck on tabs

--- js/index.js
+++ js/index.js
@@ -1,43 +1,12 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
+angular.module('tabApp', [])
+  .controller('TabController', ['$scope', function($scope) {
+    $scope.tab = 1;
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
-        } else {
-          label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
-    }
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
 
-});
-
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
-});
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+}]);
